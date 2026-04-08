450118b68d2694c0468fc9eca1b725cd52395d0b
Jar0 <jar0dev80@gmail.com>
2026-04-08 17:47:18 +0200

    add wyhy choose us

--- src/app/page.js
+++ src/app/page.js
@@ -14,6 +14,9 @@ import { Canvas } from "@react-three/fiber";
 import { Logo } from "./components/Logo/Ethereum_logo";
 
 
+// Icons
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+
 
 export default function Home() {
   return (
@@ -34,12 +37,101 @@ export default function Home() {
 
           </Container>
         </section>
+        {/* Table */}
         <section className="py-[95px] px-0">
           <Container>
             <Typography variant="h1" component='h3' align="center" gutterBottom>Today's Crypto Prices</Typography>
             <Table/>
           </Container>
         </section>
+        {/* WHY */}
+        <section className="py-[95px] px-0">
+          <Container>
+             <Typography  variant="h1" component='h3' align="center" gutterBottom> Why Choose Us</Typography>
+             <Box className='grid grid-cols-3'>
+              {/* COL_LEFT */}
+              <Box className='flex flex-col justify-between '>
+                  {/* ICOM_ONE */}
+                  <Box className='flex gap-2'>
+                      <Box className=' flex justify-center items-center bg-linear-to-r from-cyan-500 to-purple-500 p-4 rounded-md'>
+                        <AccountBalanceWalletIcon/>
+                      </Box>
+                      {/* TEXT_BOX */}
+                      <Box>
+                        <Typography variant="h5" component='h2'  >Connect your wallet</Typography>
+                        <Typography component='p'>Use trust wallet</Typography>
+                      </Box>
+                  </Box>
+                  {/* ICOM_Two */}
+                  <Box className='flex gap-2'>
+                      <Box className=' flex justify-center items-center bg-linear-to-r from-cyan-500 to-purple-500 p-4 rounded-md'>
+                        <AccountBalanceWalletIcon/>
+                      </Box>
+                      {/* TEXT_BOX */}
+                      <Box>
+                        <Typography variant="h5" component='h2'  >Connect your wallet</Typography>
+                        <Typography component='p'>Use trust wallet</Typography>
+                      </Box>
+                  </Box>
+                  {/* ICOM_THREE */}
+                  <Box className='flex gap-2'>
+                      <Box className=' flex justify-center items-center bg-linear-to-r from-cyan-500 to-purple-500 p-4 rounded-md'>
+                        <AccountBalanceWalletIcon/>
+                      </Box>
+                      {/* TEXT_BOX */}
+                      <Box>
+                        <Typography variant="h5" component='h2'  >Connect your wallet</Typography>
+                        <Typography component='p'>Use trust wallet</Typography>
+                      </Box>
+                  </Box>
+
+              </Box>
+              {/* IMG_BOX */}
+              <Box className='relative' sx={{ height: '400px' }}>
+                <Image src='/imgs/why-img.png' alt="Why choose us" fill style={{ objectFit: "contain" }}/>
+              </Box>
+              {/* COL_RIGHT */}
+              <Box className='flex flex-col justify-between '> 
+                     {/* ICOM_ONE */}
+                  <Box className='flex gap-2'>
+                      <Box className=' flex justify-center items-center bg-linear-to-r from-cyan-500 to-purple-500 p-4 rounded-md'>
+                        <AccountBalanceWalletIcon/>
+                      </Box>
+                      {/* TEXT_BOX */}
+                      <Box>
+                        <Typography variant="h5" component='h2'  >Connect your wallet</Typography>
+                        <Typography component='p'>Use trust wallet</Typography>
+                      </Box>
+                  </Box>
+                  {/* ICOM_Two */}
+                  <Box className='flex gap-2'>
+                      <Box className=' flex justify-center items-center bg-linear-to-r from-cyan-500 to-purple-500 p-4 rounded-md'>
+                        <AccountBalanceWalletIcon/>
+                      </Box>
+                      {/* TEXT_BOX */}
+                      <Box>
+                        <Typography variant="h5" component='h2'  >Connect your wallet</Typography>
+                        <Typography component='p'>Use trust wallet</Typography>
+                      </Box>
+                  </Box>
+                  {/* ICOM_THREE */}
+                  <Box className='flex gap-2'>
+                      <Box className=' flex justify-center items-center bg-linear-to-r from-cyan-500 to-purple-500 p-4 rounded-md'>
+                        <AccountBalanceWalletIcon/>
+                      </Box>
+                      {/* TEXT_BOX */}
+                      <Box>
+                        <Typography variant="h5" component='h2'  >Connect your wallet</Typography>
+                        <Typography component='p'>Use trust wallet</Typography>
+                      </Box>
+                  </Box>
+              </Box>
+             </Box>
+          </Container>
+         
+        
+        </section>
+        
       </>
   );
 }
